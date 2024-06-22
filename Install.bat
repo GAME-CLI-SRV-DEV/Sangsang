c036a86196729e9c43883041cd107218a55ae74b
Outfit8TSB <sunnybunnies09@naver.com>
2024-06-22 18:26:34 +0900

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,26 @@
+echo off
+color a
+cls
+echo Radioactive Installation DISK ][
+echo.
+echo ----------- ON ----------
+echo Radioactive 2019
+echo Supported Loader
+echo Bukkit, Spigot, Paper, Tuinity, Airplane, Purpur, Pufferfish, Plazma, Leaves, Patina, Jettpack
+echo NONE
+echo Starting Builder
+echo FARD 000000
+echo ----------- ON ----------
+echo Bukkit By Wolvereness and EvilSeph(Warren Loo), Unofficially Owned By Satya Nadella
+echo Spigot By md_5(Michael), Unofficially Owned By Mark Zuckerburg
+echo Paper By papermc 
+echo Tuinity By SpottedLeaf
+echo Airplane By TECHNOVE
+echo Purpur By granny
+echo Pufferfish By Pufferfish Host
+echo Plazma By IPECTER and Alpha93
+echo Leaves By Violetc
+echo Patina by PatinaMC
+echo Jettpack By TitaniumTown
+echo APPLYING PATCHES...
+gradlew applypatches && gradlew createreobfpaperclipjar && pause
---
+++ stop.bat
@@ -0,0 +1,6 @@
+echo off
+cls
+color 4
+echo STOP!
+gradlew --stop
+pause
