4615988a98abb9e543f12721eb81125e39996051
Outfit8TSB <sunnybunnies09@naver.com>
2024-06-25 20:45:09 +0900

    Create build.bat

---
+++ tool/build.bat
@@ -0,0 +1,9 @@
+:::::::::::::::::::::::::
+: radioactive :
+:::::::::::::::::::::::::
+echo off
+cls
+echo preparing Install Program
+git clone https://github.com/GAME-CLI-SRV-DEV/Radioactive.git
+cd radioactive
+install.bat
