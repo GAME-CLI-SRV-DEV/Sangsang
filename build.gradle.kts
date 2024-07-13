d7b71dfe2cf610d61a6a7538779d73d64d883553
Outfit8TSB <sunnybunnies09@naver.com>
2024-07-13 18:33:26 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -127,7 +127,7 @@ paperweight {
     }
 }
 
-val github = "https://api.github.com/repos/PlazmaMC/PlazmaBukkit"
+val github = "https://api.github.com/repos/GAME-CLI-SRV-DEV/PlazmaBukkit-Provisional"
 
 tasks {
     applyPatches {
